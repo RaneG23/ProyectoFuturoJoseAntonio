e8c6712f9ca3874cae1f7d800e052a2a5caa6c40
Jose Antonio Bonmatí <bonmatip@gmail.com>
2021-11-19 23:34:06 +0100

    Nuevo proyecto futuro

---
+++ Futuro/src/futuro/Futuro.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package futuro;
+
+/**
+ *
+ * @author Pepe
+ */
+public class Futuro {
+    
+    static String veredicto(String v1, String v2){
+        if (v1.equals(v2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(v1) > Integer.valueOf(v2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + " : " + veredicto(args[1],args[2]));
+    }
+    
+}
